014404a56d203131ffabacfc53eac1fe44982ed4
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2015-03-01 16:19:43 +0100

    Cyclic start

--- src/main/scala/ir/PageRank.scala
+++ src/main/scala/ir/PageRank.scala
@@ -119,7 +119,7 @@ object PageRank {
     println("Time elapsed: " + (System.nanoTime - start) * 1e-9 + " s")
   }
 
-  // Simulate N runs of the random walk initiated at a randomly chosen page
+  // Simulate N runs of the random walk initiated at a randomly chosen page.
   def monteCarloRandomStart() {
     val start = System.nanoTime
     val N = documentMap.size
@@ -127,24 +127,8 @@ object PageRank {
 
     var x = new Array[Double](N)
 
-    for (walk <- 0 until N) {
-      var page = r.nextInt(N)
-
-      breakable {
-        while (true) {
-          val prob = r.nextDouble
-          if (prob >= 1 - α) {
-            if (A.contains(page)) {
-              page = A(page).toList(r.nextInt(A(page).size)) // TODO
-            } else {
-              page = r.nextInt(N)
-            }
-          } else {
-            break
-          }
-        }
-      }
- 
+    for (n <- 0 until N) {
+      var page = walk(r.nextInt(N))
       x(page) += 1
     }
 
@@ -157,11 +141,58 @@ object PageRank {
     println("Time elapsed: " + (System.nanoTime - start) * 1e-9 + " s")
   }
 
+  // Simulate N = mn runs of the random walk initiated at 
+  // each page exactly m times.
+  def monteCarloCyclicStart() {
+    val start = System.nanoTime
+    val N = documentMap.size
+    val M = 50
+    val r = scala.util.Random
+
+    var x = new Array[Double](N)
+
+    for (n <- 0 until N) {
+      var page = r.nextInt(N)
+      for (m <- 0 until M) {
+        x(walk(page)) += 1
+      }
+    }
+
+    println("Result:")
+    x.map(_ / N)
+      .zipWithIndex
+      .sortBy(-_._1)
+      .take(50)
+      .foreach((x) => println(documentMap(x._2) + " " + x._1))
+    println("Time elapsed: " + (System.nanoTime - start) * 1e-9 + " s")
+  }
+
+  def walk(page: Int): Int = {
+    // TODO
+    val N = documentMap.size
+    val r = scala.util.Random
+    var p = page
+    while (true) {
+      val prob = r.nextDouble
+      if (prob >= 1 - α) {
+        if (A.contains(p)) {
+          p = A(p).toList(r.nextInt(A(p).size)) // TODO
+        } else {
+          p = r.nextInt(N)
+        }
+      } else {
+        return p
+      }
+    }
+    0
+  }
+
   def main(args: Array[String]) {
     println("Reading links")
     readFile("./links.txt")
     //powerIterate()
-    monteCarloRandomStart()
+    //monteCarloRandomStart()
+    monteCarloCyclicStart()
   }
 }
 
